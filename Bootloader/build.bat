a7707529d6a722a9548e1ce993de6bf760027b39
László Dániel <laszlodaniel@gmail.com>
2022-05-23 09:25:15 +0200

    Rename files

--- Bootloader/build.bat
+++ Bootloader/build.bat
@@ -1,15 +1,15 @@
-asm68hc16 -o 01_LdBoot_128k.hex 01_LdBoot_128k.asm
-asm68hc16 -o 01_LdBoot_256k.hex 01_LdBoot_256k.asm
-asm68hc16 -o 02_LdPartNumberRead.hex 02_LdPartNumberRead.asm
-asm68hc16 -o 10_LdFlashRead.hex 10_LdFlashRead.asm
-asm68hc16 -o 11_LdFlashID.hex 11_LdFlashID.asm
-hex2bin 01_LdBoot_128k.hex
-hex2bin 01_LdBoot_256k.hex
-hex2bin 02_LdPartNumberRead.hex
-hex2bin 10_LdFlashRead.hex
-hex2bin 11_LdFlashID.hex
-bin2header 01_LdBoot_128k.bin LdBoot_128k > 01_LdBoot_128k.h
-bin2header 01_LdBoot_256k.bin LdBoot_256k > 01_LdBoot_256k.h
-bin2header 02_LdPartNumberRead.bin LdPartNumberRead > 02_LdPartNumberRead.h
-bin2header 10_LdFlashRead.bin LdFlashRead > 10_LdFlashRead.h
-bin2header 11_LdFlashID.bin LdFlashID > 11_LdFlashID.h
+asm68hc16 -o LdBoot_128k_SBEC3.hex LdBoot_128k_SBEC3.asm
+asm68hc16 -o LdBoot_256k_SBEC3.hex LdBoot_256k_SBEC3.asm
+asm68hc16 -o LdPartNumberRead_SBEC3.hex LdPartNumberRead_SBEC3.asm
+asm68hc16 -o LdFlashRead_SBEC3.hex LdFlashRead_SBEC3.asm
+asm68hc16 -o LdFlashID_SBEC3.hex LdFlashID_SBEC3.asm
+hex2bin LdBoot_128k_SBEC3.hex
+hex2bin LdBoot_256k_SBEC3.hex
+hex2bin LdPartNumberRead_SBEC3.hex
+hex2bin LdFlashRead_SBEC3.hex
+hex2bin LdFlashID_SBEC3.hex
+bin2header LdBoot_128k_SBEC3.bin LdBoot_128k_SBEC3 > LdBoot_128k_SBEC3.h
+bin2header LdBoot_256k_SBEC3.bin LdBoot_256k_SBEC3 > LdBoot_256k_SBEC3.h
+bin2header LdPartNumberRead_SBEC3.bin LdPartNumberRead_SBEC3 > LdPartNumberRead_SBEC3.h
+bin2header LdFlashRead_SBEC3.bin LdFlashRead_SBEC3 > LdFlashRead_SBEC3.h
+bin2header LdFlashID_SBEC3.bin LdFlashID_SBEC3 > LdFlashID_SBEC3.h
